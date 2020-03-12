1f883bca22e5fc90ce23f8155eae93a8644742e7
Joshua Sherman <jsherman@valcom.com>
2020-03-12 15:16:56 -0400

    removes unit tests not relevant

--- test/containers/Homepage.spec.js
+++ test/containers/Homepage.spec.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Homepage } from '../../src/containers/Homepage';
 import WideAboutUs from '../../src/containers/Homepage/Widescreen/WideAbout';
-import WideFacebookFeed from '../../src/containers/Homepage/Widescreen/WideFacebookFeed';
 
 const wrapper = shallow(<Homepage />);
 
@@ -12,16 +11,9 @@ describe('Home', () => {
     wrapper.instance().forceUpdate();
     expect(wrapper.find(WideAboutUs).exists()).toBe(true);
     expect(wrapper.find(WideAboutUs).dive().find('div.widescreenHomepage').exists()).toBe(true);
-    expect(wrapper.find(WideFacebookFeed).dive()
-      .find('div')
-      .exists()).toBe(true);
   });
   it('Resizes the page', () => {
     wrapper.instance().onResize(100);
     expect(wrapper.instance().state.width).toBe(100);
-  });
-  it('Renders WideFacebook when at least 1092', () => {
-    const wrapper2 = shallow(<WideFacebookFeed width={1092} />);
-    expect(wrapper2.find('p#wideFacebook').prop('style')).toHaveProperty('marginBottom', '32px');
   });
 });
