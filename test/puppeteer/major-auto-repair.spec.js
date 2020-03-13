e70f8d905842ba63ef3d88c869c9389731e7a12d
Joshua Sherman <jsherman@valcom.com>
2020-03-12 22:18:08 -0400

    fixes puppeteer tests

--- test/puppeteer/major-auto-repair.spec.js
+++ test/puppeteer/major-auto-repair.spec.js
@@ -2,9 +2,9 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 describe('Music', () => {
-  it('should be titled "Music | Web Jam LLC"', async () => {
+  it('has a page with title of Major Auto Repair | Apperson Automotive"', async () => {
     const page = await browser.newPage();
-    await page.goto('http://localhost:8888/music', { waitUntil: 'load' });
+    await page.goto('http://localhost:8888/major-auto-repair', { waitUntil: 'load' });
     await expect(page.title()).resolves.toMatch('Major Auto Repair | Apperson Automotive');
   });
 });
