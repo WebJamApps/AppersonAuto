57b131e91f492e4f1a503e8b23c5c5e6ed2edaa6
Rhayven <codingforllamas@gmail.com>
2020-03-17 06:32:30 -0400

    Adds contact content and styling changes

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -1,20 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import DefaultYouthContent from './YouthContent';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+import React, { Component } from 'react';
+import ReactResizeDetector from 'react-resize-detector';
+import DefaultWideMap from './WideMap';
+import DefaultNarrowMap from './NarrowMap';
 import commonUtils from '../../lib/commonUtils';
 
-export const Youth = ({ youthPics }) => {
-  commonUtils.setTitleAndScroll('Youth Ministry');
-  return (<DefaultYouthContent youthPics={youthPics} />);
-};
+export default class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.parentRef = React.createRef();
+    this.onResize = this.onResize.bind(this);
+    this.state = { width: 320 };
+    this.commonUtils = commonUtils;
+  }
 
-Youth.defaultProps = { youthPics: [] };
-Youth.propTypes = {
-  youthPics: PropTypes.arrayOf(PropTypes.shape({
-    comments: PropTypes.string,
-  })),
-};
+  componentDidMount() { this.commonUtils.setTitleAndScroll('', window.screen.width); }
 
-export default connect(mapStoreToProps, null)(Youth);
+  onResize(width) {
+    this.setState({ width });
+  }
+
+  render() {
+    const { width } = this.state;
+    return (
+      <div>
+        {width >= 1004
+          ? (
+            <div className="page-content">
+              <DefaultWideMap />
+              <p style={{ fontSize: '6pt', marginBottom: '0' }}>&nbsp;</p>
+            </div>
+          )
+          : (
+            <div className="page-content">
+              <DefaultNarrowMap />
+            </div>
+          )}
+        <ReactResizeDetector handleWidth handleHeight onResize={this.onResize} targetDomEl={this.parentRef.current} />
+      </div>
+    );
+  }
+}
