b5469ebb2940f0a34146d1017ca1df524852e1c7
Joshua Sherman <jsherman@valcom.com>
2020-03-24 10:43:59 -0400

    finishes unit tests

--- src/redux/allReducers.js
+++ src/redux/allReducers.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import imagesReducer from './reducers/imagesReducer';
 import authReducer from './reducers/authReducer';
-import homeReducer from './reducers/homeReducer';
 import familyReducer from './reducers/familyReducer';
 import youthReducer from './reducers/youthReducer';
 import booksReducer from './reducers/booksReducer';
@@ -9,7 +8,6 @@ import booksReducer from './reducers/booksReducer';
 const reducer = combineReducers({
   images: imagesReducer,
   auth: authReducer,
-  homeContent: homeReducer,
   familyPics: familyReducer,
   youthPics: youthReducer,
   books: booksReducer,
