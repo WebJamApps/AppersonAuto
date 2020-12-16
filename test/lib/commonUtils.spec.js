ed639510abe2129524b24b5cd28d02623e707365
Rhayven <codingforllamas@gmail.com>
2020-12-16 00:03:43 -0500

    Test fixes

--- test/lib/commonUtils.spec.js
+++ test/lib/commonUtils.spec.js
@@ -7,7 +7,7 @@ describe('commonUtils', () => {
   it('calls scrollIntoView', () => {
     const scrollIntoViewMock = jest.fn();
     window.HTMLElement.prototype.scrollIntoView = scrollIntoViewMock;
-    document.body.innerHTML = '<div class="container-fluid"></div>';
+    document.body.innerHTML = '<div class="page-content"></div>';
     commonUtils.setTitleAndScroll('home');
     expect(scrollIntoViewMock).toHaveBeenCalled();
   });
