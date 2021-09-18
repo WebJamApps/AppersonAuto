44a3d5e00d3627855ee44b68ec4b377784fe6123
Rhayven <codingforllamas@gmail.com>
2021-09-18 04:06:25 -0400

    Cleanup and mobile fixes

--- src/lib/commonUtils.tsx
+++ src/lib/commonUtils.tsx
@@ -4,7 +4,7 @@ const setTitleAndScroll = (pageTitle: string, width: number): void => {
   if (pageTitle !== '') pageTitle += ' | ';// eslint-disable-line no-param-reassign
   document.title = `${pageTitle}Apperson Automotive`;
   let getClass = 'top';
-  if (width !== undefined && width < 1004) getClass = 'headercontent';
+  if (width !== undefined && width < 1004) getClass = 'material-header';
   const top = document.getElementsByClassName(getClass)[0];
   if (top !== undefined && typeof top.scrollIntoView === 'function') top.scrollIntoView();
 };
