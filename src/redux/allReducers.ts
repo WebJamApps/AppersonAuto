5667539a3b579c0581c31ff8a3d9aecf521e4777
Rhayven <codingforllamas@gmail.com>
2021-09-18 03:44:04 -0400

    Removes unused reducer

--- src/redux/allReducers.ts
+++ src/redux/allReducers.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import imagesReducer from './reducers/imagesReducer';
 import authReducer from './reducers/authReducer';
-import booksReducer from './reducers/booksReducer';
 
 const reducer = combineReducers({
   images: imagesReducer,
   auth: authReducer,
-  books: booksReducer,
 });
 
 export default reducer;
